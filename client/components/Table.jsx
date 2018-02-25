55d540c03a35ced33f5e6f71c6e9a735d468aafd
iamkennytso <iamkennytso@gmail.com>
2018-02-25 16:23:04 -0500

    (feat) table stateful component

--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
-import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Table, { TableBody, TableCell, TableHead, TableRow} from 'material-ui/Table';
 
-const table = (props) => {
+class table extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      orderBy: 'release',
+      order: 'dec'
+    }
+  }
+  
+  render() {
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell style={{ 'fontWeight': 'bold' }}> Album Art  </TableCell>
-          <TableCell style={{ 'fontWeight': 'bold' }}> Album Name </ TableCell>
-          <TableCell style={{ 'fontWeight': 'bold' }}> Release Date </ TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {
-          props.albums.map(album => {
-            let releaseDate = album.release
-            return (
-              <TableRow key={album.link}>
-                <TableCell > <img src={album.art} /> </TableCell>
-                <TableCell > <a href={album.link}>{album.name}</a> </TableCell>
-                {/* Eventually want to make this table sortable by user input */}
-                <TableCell > {releaseDate.getMonth()+1}/{releaseDate.getDate()}/{String(releaseDate.getFullYear()).slice(2,4)} </TableCell>
-              </TableRow>
-            )
-          })
-        }
-      </TableBody>
-    </Table>
-  )
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell style={{ 'fontWeight': 'bold' }}> Album Art  </TableCell>
+            <TableCell style={{ 'fontWeight': 'bold' }}> Album Name </ TableCell>
+            <TableCell style={{ 'fontWeight': 'bold' }}> Release Date </ TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            this.props.albums.map(album => {
+              let releaseDate = album.release
+              return (
+                <TableRow key={album.link}>
+                  <TableCell > <img src={album.art} /> </TableCell>
+                  <TableCell > <a href={album.link}>{album.name}</a> </TableCell>
+                  {/* Eventually want to make this table sortable by user input */}
+                  <TableCell > {releaseDate.getMonth()+1}/{releaseDate.getDate()}/{String(releaseDate.getFullYear()).slice(2,4)} </TableCell>
+                </TableRow>
+              )
+            })
+          }
+        </TableBody>
+      </Table>
+    )
+  }
 }
 
 export default table;
